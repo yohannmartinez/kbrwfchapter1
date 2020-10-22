91aade9c10a91ee51c7deecb56d72732336df96f
yohannmartinez <yohannmartinez34@gmail.com>
2020-10-22 19:11:19 +0200

    step 5 bloqued

--- lib/server/database.ex
+++ lib/server/database.ex
@@ -1,4 +1,4 @@
-defmodule KbrwFormation.Database do 
+defmodule KbrwFormation.Database do
   use GenServer
 
   def init do
@@ -29,6 +29,46 @@ defmodule KbrwFormation.Database do
 
   def update(key, value) do
     :ets.update_element(:wrapper, key, {2, value})
+  end
+
+  def search(filter_elements) do
+    search_results = []
+    case Enum.count(filter_elements) do
+      #checking number of search filters and if 0 reply nil
+      0 ->
+        []
+      _ ->
+        #if search filters length > 0
+        Enum.map(filter_elements, fn(filter_element) ->
+          #key and value filters
+          search_filter_key = elem(filter_element, 0)
+          search_filter_value = elem(filter_element, 1)
+          #retrieve all elements of ets table
+          datas = :ets.tab2list(:wrapper)
+
+          #check in each element of ets table if search filter exist
+          Enum.map(datas, fn(data) ->
+            new_data = Enum.at(Tuple.to_list(data), 1);
+            case Map.has_key?(new_data, search_filter_key) and search_filter_value == new_data[search_filter_key] do
+            #if filter match add order to search_results list
+            true ->
+              order_id = Enum.at(Tuple.to_list(data), 0)
+
+            #last check to see if order doesn't already exist in search_results list
+              case Enum.filter(search_results, fn(item) -> item["id"] == order_id end) do
+                [] ->
+                  IO.inspect(search_results)
+                  List.insert_at(search_results, 0, data)
+                _ ->
+                  nil
+              end
+            false ->
+              nil
+            end
+          end)
+        end)
+    end
+    search_results
   end
 
 end
